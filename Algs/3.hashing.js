2fd77815c0b65b76711169cb161966a063cb8ab6
VasylArtymovych <art777vasss@gmail.com>
2022-12-23 10:31:29 +0200

    add hashing and cillising

--- Algs/3.hashing.js
+++ Algs/3.hashing.js
@@ -1,7 +1,14 @@
-class Hashing {
-  constructor(capacity) {
-    this.store = new Array(capacity);
-    this.size = capacity;
+// Implementation of hashing using linkedLIst:
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.next = null;
+  }
+}
+
+class HashTableChaining {
+  constructor(size) {
+    this.store = new Array(size).fill(null);
   }
 
   hash(key) {
@@ -9,46 +16,103 @@ class Hashing {
     for (let i = 0; i < key.length; i++) {
       sum += key.charCodeAt(i);
     }
-    return sum % this.size;
+    return sum % this.store.length;
   }
 
-  add(key, value) {
-    this.store[this.hash(key)] = this.store[this.hash(key)] || [];
-    this.store[this.hash(key)].push({ key, value });
-  }
-
-  get(key) {
-    if (!this.store[this.hash(key)]) {
-      return null;
+  insert(key, value) {
+    const newNode = new Node({ key, value });
+    const chain = this.hash(key);
+    if (this.store[chain] === null) {
+      this.store[chain] = newNode;
     } else {
-      return this.store[this.hash(key)].find((item) => item.key === key)?.value;
+      let temp = this.store[chain];
+      while (temp.next !== null) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
     }
   }
 
   delete(key) {
-    if (!this.store[this.hash(key)]) {
-      return null;
-    } else {
-      let index = this.store[this.hash(key)].findIndex(
-        (item) => item.key === key
-      );
-      if (index !== -1) {
-        this.store[this.hash(key)].splice(index, 1);
-        return `Key: ${key} was deleted`;
+    const chain = this.hash(key);
+    let temp = this.store[chain];
+
+    if (temp !== null) {
+      if (temp.data.key === key) {
+        this.store[chain] = temp.next;
+        return 1;
       } else {
-        return null;
+        while (temp.next !== null) {
+          if (temp.next.data.key === key) {
+            temp.next = temp.next.next;
+            return 1;
+          }
+          temp = temp.next;
+        }
       }
+    }
+    return 0;
+  }
+
+  find(key) {
+    const chain = this.hash(key);
+    let temp = this.store[chain];
+    while (temp !== null) {
+      if (temp.data.key === key) {
+        return temp.data.value;
+      }
+      temp = temp.next;
+    }
+    return null;
+  }
+
+  print() {
+    for (let key in this.store) {
+      let temp = this.store[key];
+      document.write(`chain[${key}] --> `);
+      while (temp !== null) {
+        document.write(`${temp.data.value} --> `);
+        temp = temp.next;
+      }
+      document.write(`${null}<br>`);
     }
   }
 }
 
-const ht = new Hashing(7);
-// console.log(ht);
-// ht.add('ab', 11);
-// ht.add('a', 12);
-// ht.add('ac', 22);
-// ht.add('ad', 33);
-// console.log(ht.get('ad'));
-// console.log(ht.get('a'));
-// console.log(ht.delete('ad'));
-// console.log(ht.delete('a'));
+var htc = new HashTableChaining(5);
+// htc.insert('ba', 1);
+// htc.insert('bb', 2);
+// htc.insert('cc', 3);
+// htc.insert('ab', 4);
+// htc.insert('a', 5);
+// htc.insert('aa', 7);
+// htc.insert('abc', 8);
+// htc.delete('ab');
+// console.log(htc.find('aa'));
+// console.log(htc);
+// htc.print();
+
+// Implement hash table with linear probing - Insert:________________________-
+const hTable = new Array(5).fill(-1);
+const insert = (value) => {
+  const key = value % hTable.length;
+  let index = key;
+
+  while (hTable[index] !== -1) {
+    index = (index + 1) % hTable.length;
+    if (index === key) {
+      return console.log('hTable is full!');
+    }
+  }
+
+  hTable[index] = value;
+};
+
+insert(1);
+insert(2);
+insert(3);
+insert(4);
+insert(5);
+insert(1);
+
+console.log('hTable', hTable);
--- DS/10.hash_table.js
+++ DS/10.hash_table.js
@@ -1,3 +1,4 @@
+// Implementation using Array: _______________________________________________
 class HashTable {
   constructor(capacity) {
     this.store = new Array(capacity);
@@ -42,13 +43,107 @@ class HashTable {
   }
 }
 
-const ht = new HashTable(7);
-console.log(ht);
-ht.add('ab', 11);
-ht.add('a', 12);
-ht.add('ac', 22);
-ht.add('ad', 33);
-console.log(ht.get('ad'));
-console.log(ht.get('a'));
-console.log(ht.delete('ad'));
-console.log(ht.delete('a'));
+// const ht = new HashTable(7);
+// console.log(ht);
+// ht.add('ab', 11);
+// ht.add('a', 12);
+// ht.add('ac', 22);
+// ht.add('ad', 33);
+// console.log(ht.get('ad'));
+// console.log(ht.get('a'));
+// console.log(ht.delete('ad'));
+// console.log(ht.delete('a'));
+
+// Implementation using Linked List: ______________________________________________________
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.next = null;
+  }
+}
+
+class HashTableChaining {
+  constructor(size) {
+    this.store = new Array(size).fill(null);
+  }
+
+  hash(key) {
+    let sum = 0;
+    for (let i = 0; i < key.length; i++) {
+      sum += key.charCodeAt(i);
+    }
+    return sum % this.store.length;
+  }
+
+  insert(key, value) {
+    const newNode = new Node({ key, value });
+    const chain = this.hash(key);
+    if (this.store[chain] === null) {
+      this.store[chain] = newNode;
+    } else {
+      let temp = this.store[chain];
+      while (temp.next !== null) {
+        temp = temp.next;
+      }
+      temp.next = newNode;
+    }
+  }
+
+  delete(key) {
+    const chain = this.hash(key);
+    let temp = this.store[chain];
+
+    if (temp !== null) {
+      if (temp.data.key === key) {
+        this.store[chain] = temp.next;
+        return 1;
+      } else {
+        while (temp.next !== null) {
+          if (temp.next.data.key === key) {
+            temp.next = temp.next.next;
+            return 1;
+          }
+          temp = temp.next;
+        }
+      }
+    }
+    return 0;
+  }
+
+  find(key) {
+    const chain = this.hash(key);
+    let temp = this.store[chain];
+    while (temp !== null) {
+      if (temp.data.key === key) {
+        return temp.data.value;
+      }
+      temp = temp.next;
+    }
+    return null;
+  }
+
+  print() {
+    for (let key in this.store) {
+      let temp = this.store[key];
+      document.write(`chain[${key}] --> `);
+      while (temp !== null) {
+        document.write(`${temp.data.value} --> `);
+        temp = temp.next;
+      }
+      document.write(`${null}<br>`);
+    }
+  }
+}
+
+// var htc = new HashTableChaining(5);
+// htc.insert('ba', 1);
+// htc.insert('bb', 2);
+// htc.insert('cc', 3);
+// htc.insert('ab', 4);
+// htc.insert('a', 5);
+// htc.insert('aa', 7);
+// htc.insert('abc', 8);
+// htc.delete('ab');
+// console.log(htc.find('aa'));
+// console.log(htc);
+// htc.print();
